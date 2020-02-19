90ed1e2f9d39c60abd08f42e78764082377ca76a
soerjo <ryohastomo@gmail.com>
2020-02-19 22:44:34 +0700

    hapus script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let nama = "Suryo Hastomo";
-let umur = 23;
-
-console.log(`halo nama gue ${nama}, gue berumum ${umur} tahun.`);
-console.log("mari kita berjuang bersama!");
-console.log("gue akan terub berjuang");
